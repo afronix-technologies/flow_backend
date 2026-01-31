7a3c83555cb75171776f89eca108d35fb9531463
Triumph <emrctechteam@gmail.com>
2026-01-31 21:52:48 +0100

    fix: update redis.config.ts to use cache-manager-redis-yet instead of cache-manager-redis-store

--- src/core/config/redis.config.ts
+++ src/core/config/redis.config.ts
@@ -1,12 +1,13 @@
 import { CacheModuleAsyncOptions } from '@nestjs/cache-manager';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { redisStore } from 'cache-manager-redis-store';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { redisStore } = require('cache-manager-redis-yet');
 
 export const redisConfig: CacheModuleAsyncOptions = {
     imports: [ConfigModule],
     inject: [ConfigService],
     useFactory: async (configService: ConfigService) => ({
-        store: redisStore as any,
+        store: redisStore,
         host: configService.get<string>('REDIS_HOST'),
         port: configService.get<number>('REDIS_PORT'),
         ttl: configService.get<number>('REDIS_TTL'),
